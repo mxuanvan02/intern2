342633d9fbe16269135c3e323c6a939c971a0b9d
mxuanvan02 <mxuanvan159@gmail.com>
2023-04-07 20:48:47 +0700

    Su dung async await va toi uu code.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Articles from './components/articles';
 
 function App() {
+  const [articles, setArticles] = useState([])
+  var url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5b2a127c4bf1411c8a3de122fd6c7fde"
+  useEffect(() => {
+    const getArticles = async () => {
+      const articlesFromServer = await fetchArticles()
+      setArticles(articlesFromServer.articles)
+    }
+    getArticles()
+  }, []);
+  const fetchArticles = async () => {
+    const res = await fetch(url)
+    const data = await res.json()
+    return data
+  }
+  console.log(articles)
   return (
-    <div className="App">
-      <Articles />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Articles articles={articles}/>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/articles/index.js
+++ src/components/articles/index.js
@@ -1,37 +1,13 @@
 import React from 'react'
 import './index.css';
-import { useState, useEffect } from 'react';
+import Article from '../article';
 
-const Articles = () => {
-  const [articles, setArticles] = useState([])
-  var url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5b2a127c4bf1411c8a3de122fd6c7fde"
-  const fetchArticles = () => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setArticles(data.articles));
-  }
-  useEffect(() => {
-    fetchArticles();
-  }, []);
-  console.log(articles)
+const Articles = ({ articles }) => {
 
   return (
     <div className="group-news-other">
       <ul className="other-news" id="list-latest-news">
-        {articles.map((article, i) => 
-          <li id="xahoi2023-02-27" key={i}>
-            <div className="other-news-item">
-              <img src={article.urlToImage} alt={article.title} className="news-img" />
-              <div className="other-news-contents">
-                <a className="news-title" href={article.url}><b>{article.title}</b></a>
-                <p className="news-author">{article.author}</p>
-                <time className="news-date" dateTime={article.publishedAt}>{article.publishedAt}</time>
-                <p className="short-contents-other">{article.content}</p>
-              </div>
-            </div>
-            <p className="short-contents-other-mobile">{article.content}<hr className="hr-mobile" /></p>
-          </li>
-        )}
+        {articles.map((article, i) => (<Article key={i} article={article} />))}
       </ul>
     </div>
   )
