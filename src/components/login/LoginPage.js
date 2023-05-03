285a1e1c5ba6ac4be1abe832fdc544ff28d72412
nhan <nhanpham.190302@gmail.com>
2023-05-03 16:21:49 +0700

    fix login

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,41 +1,42 @@
+import { Link } from "react-router-dom";
 
 const  LoginPage  = () => {
     return ( 
-        <div class="flex-container login__wrap">
-        <form class="login-form flex-container" id="loginForm" >
-          <div class="flex-row">
-            <h1 class="login__title">Đăng nhập</h1>
-            <div class="login-form__item">
-              <label class="login-form__label">Tên đăng nhập</label>
+        <div className="flex-container login__wrap">
+        <form className="login-form flex-container" id="loginForm" >
+          <div className="flex-row">
+            <h1 className="login__title">Đăng nhập</h1>
+            <div className="login-form__item">
+              <label className="login-form__label">Tên đăng nhập</label>
               <br />
               <input 
               type="text" 
               id="userUserName" 
               placeholder="Tên đăng nhập" 
-              class="login-form__control" 
+              className="login-form__control" 
               
               required/>
             </div>
-            <div class="login-form__item">
-              <label class="login-form__label">Mật khẩu</label>
+            <div className="login-form__item">
+              <label className="login-form__label">Mật khẩu</label>
               <br />
               <input 
               type="password" 
               id="userPassword" 
               placeholder="Mật khẩu" 
-              class="login-form__control" 
+              className="login-form__control" 
               
               required />
             </div>
-            <div class="login-form__check">
-            <input type="checkbox" id="rememberMe" value="IsRememberMe" class="login-form__check-input"/>
-              <label class="form-check-label">Nhớ tên đăng nhập</label>
+            <div className="login-form__check">
+            <input type="checkbox" id="rememberMe" value="IsRememberMe" className="login-form__check-input"/>
+              <label className="form-check-label">Nhớ tên đăng nhập</label>
             </div>
-            <div class="text-center"><button type="submit" id="login_btn" value="login" onclick="onLogin()" class="btn login-form__btn">Đăng nhập</button></div>
-            <div class="login-form__redirect-wrap">
-              <div class="login-form__to-main-page"><a href="/">Đến trang chủ.</a></div>
-              <div class="login-form__forgot"><a href="#">Quên mật khẩu?</a></div>
-              <div class="login-form__register"><a href="/register">Đăng ký tài khoản.</a></div>
+            <div className="text-center"><button type="submit" id="login_btn" value="login" onclick="onLogin()" className="btn login-form__btn">Đăng nhập</button></div>
+            <div className="login-form__redirect-wrap">
+              <div className="login-form__to-main-page"><Link to="/">Đến trang chủ.</Link></div>
+              <div className="login-form__forgot"><Link to="#">Quên mật khẩu?</Link></div>
+              <div className="login-form__register"><Link to="/register">Đăng ký tài khoản.</Link></div>
             </div>
           </div>
         </form>
--- src/components/register/RegisterPage.js
+++ src/components/register/RegisterPage.js
@@ -1,45 +1,46 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 const  RegisterPage  = () => {
     return ( 
-        <div class="flex-container register__wrap">
-        <form id="registerForm" class="register-form flex-container">
-        <div class="flex-row">
-          <h1 class="register__title">Đăng ký</h1>
-          <div class="register-form__item">
-            <label class="register-form__label">Họ và tên</label>
+        <div className="flex-container register__wrap">
+        <form id="registerForm" className="register-form flex-container">
+        <div className="flex-row">
+          <h1 className="register__title">Đăng ký</h1>
+          <div className="register-form__item">
+            <label className="register-form__label">Họ và tên</label>
             <br />
-            <input type="text" id="name" placeholder="Họ và tên" class="register-form__control" required></input>
+            <input type="text" id="name" placeholder="Họ và tên" className="register-form__control" required></input>
           </div>
-          <div class="register-form__item">
-            <label class="register-form__label" >Email</label>
+          <div className="register-form__item">
+            <label className="register-form__label" >Email</label>
             <br />
-            <input type="email" id="email" placeholder="Email" class="register-form__control" required></input>
+            <input type="email" id="email" placeholder="Email" className="register-form__control" required></input>
           </div>
-          <div class="register-form__item">
-            <label class="register-form__label">Tên đăng nhập</label>
+          <div className="register-form__item">
+            <label className="register-form__label">Tên đăng nhập</label>
             <br />
-            <input type="text" id="userName" placeholder="Tên đăng nhập" class="register-form__control" required></input>
+            <input type="text" id="userName" placeholder="Tên đăng nhập" className="register-form__control" required></input>
           </div>
-          <div class="register-form__item">
-            <label class="register-form__label">Mật khẩu</label>
+          <div className="register-form__item">
+            <label className="register-form__label">Mật khẩu</label>
             <br />
-            <input type="password" id="password" placeholder="Mật khẩu" class="register-form__control" required></input>
+            <input type="password" id="password" placeholder="Mật khẩu" className="register-form__control" required></input>
           </div>
-          <div class="register-form__item">
-            <label class="register-form__label">Nhập lại mật khẩu</label>
+          <div className="register-form__item">
+            <label className="register-form__label">Nhập lại mật khẩu</label>
             <br />
-            <input type="password" id="repeatPassword" placeholder="Nhập lại mật khẩu" class="register-form__control" required></input>
+            <input type="password" id="repeatPassword" placeholder="Nhập lại mật khẩu" className="register-form__control" required></input>
           </div>
-          <ul class="helper-text">
-            <li class="helper-text__length">Mật khẩu phải chứa ít nhất 8 kí tự.</li>
-            <li class="helper-text__lowercase">Mật khẩu phải chứa ít nhất 1 kí tự thường.</li>
-            <li class="helper-text__uppercase">Mật khẩu phải chứa ít nhất 1 kí tự hoa.</li>
-            <li class="helper-text__special">Mật khẩu phải chứa ít nhất 1 kí tự số.</li>
+          <ul className="helper-text">
+            <li className="helper-text__length">Mật khẩu phải chứa ít nhất 8 kí tự.</li>
+            <li className="helper-text__lowercase">Mật khẩu phải chứa ít nhất 1 kí tự thường.</li>
+            <li className="helper-text__uppercase">Mật khẩu phải chứa ít nhất 1 kí tự hoa.</li>
+            <li className="helper-text__special">Mật khẩu phải chứa ít nhất 1 kí tự số.</li>
           </ul>
-          <div class="text-center"><button type="submit" id="btn_register" value="register" onclick="onRegister()" class="btn register-form__btn">Đăng ký</button></div>
-          <div class="register-form__redirect-wrap">
-            <div class="register-form__to-main-page"><a href="/">Đến trang chủ.</a></div>
-            <div class="register-form__login"><a href="/login">Đã có tài khoản?</a></div>
+          <div className="text-center"><button type="submit" id="btn_register" value="register" onclick="onRegister()" className="btn register-form__btn">Đăng ký</button></div>
+          <div className="register-form__redirect-wrap">
+            <div className="register-form__to-main-page"><Link to="/">Đến trang chủ.</Link></div>
+            <div className="register-form__login"><Link to="/login">Đã có tài khoản?</Link></div>
           </div>
         </div>
       </form>
